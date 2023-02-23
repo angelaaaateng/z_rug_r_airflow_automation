0ef07497bf0d94d12684afd9a497b665431a1f8d
Amber Teng <31015696+angelaaaateng@users.noreply.github.com>
2023-02-23 13:21:35 +0800

    add new cron job

--- scheduler.R
+++ scheduler.R
@@ -1,12 +1,13 @@
 # for windows 
 # install.packages("taskscheduleR")
 # library(taskscheduleR)
-# install.packages("cronR")
+install.packages("cronR",  repos = "http://cran.us.r-project.org")
 library(cronR)
 sample_script <- "/Users/angelateng/Documents/GitHub/zlift_internal_airflow/sampleRscript.R"
 cmd <- cron_rscript(sample_script)
 cron_add(command = cmd, frequency = 'minutely', 
-         id = 'test1', description = 'basic loading and printing of data', tags = c('lab', 'xyz'))
+         id = 'test2', description = 'basic loading and printing of data', tags = c('lab', 'xyz'))
 cron_njobs()
 
 cron_ls()
+cron_clear(ask = TRUE)
