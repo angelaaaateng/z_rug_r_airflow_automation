2e78934817a17eaada1c48925432a93df12924ef
Amber Teng <31015696+angelaaaateng@users.noreply.github.com>
2023-02-23 13:08:18 +0800

    add cronr scheduler

--- scheduler.R
+++ scheduler.R
@@ -3,3 +3,10 @@
 # library(taskscheduleR)
 install.packages("cronR")
 library(cronR)
+sample_script <- "/Users/angelateng/Documents/GitHub/zlift_internal_airflow/sampleRscript.R"
+cmd <- cron_rscript(sample_script)
+cron_add(command = cmd, frequency = 'minutely', 
+         id = 'test1', description = 'basic loading and printing of data', tags = c('lab', 'xyz'))
+cron_njobs()
+
+cron_ls()
