42d04faff7251233b3b281a1e33545620770bb11
Amber Teng <31015696+angelaaaateng@users.noreply.github.com>
2023-02-23 13:48:29 +0800

    add counts by age pic

---
+++ airflow_home/scripts/C_task.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+
+# Load data
+data <- read.csv("/Users/angelateng/Documents/GitHub/zlift_internal_airflow/users.csv", header=TRUE)
+
+# Barplot
+p <- ggplot(data, aes(x = as.factor(dob.age))) +
+  geom_bar(stat = "count", position = "stack", fill = "#FF6666") +
+  labs(x = "Age",
+       y = "Count")
+
+png("/Users/angelateng/Documents/GitHub/zlift_internal_airflow/counts_by_age.png", width = 800, height = 400)
+print(p)
+dev.off()
